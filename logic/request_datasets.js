b2e1dace26f32b5454e639c72ac7537ef7880962
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-30 10:49:07 -0500

    Implement method for request information about rent.

--- logic/request_datasets.js
+++ logic/request_datasets.js
@@ -48,3 +48,64 @@ function show_datasets_noaa_api() {
     });
 
 }
+
+
+
+/*
+ * This function consume the apartments avaliable for renting
+ */
+
+function show_datasets_rent(){
+  let results = response
+  dataset = [];
+
+  // Iterate over the data in the response
+  results.forEach(function (e) {
+
+    if ("undefined" === typeof e.location){
+      //nothing
+    }else{  //Just storage those who has location
+      var loc = [e.location.coordinates[0], e.location.coordinates[1]]
+      var row = [e.address, e.community_area, e.phone_number, e.property_name, e.zip_code, loc[0], loc[1]]
+      dataset.push(row)
+    }
+
+  });
+
+  // Print the dataset in console
+  console.log(dataset)
+
+  //Id table
+  let id_table = "table_datasets"
+
+  // Creating the table
+  let table = $('#' + id_table)
+    .DataTable({
+      data: dataset,
+      columns: [{
+        title: "Address"
+    }, {
+        title: "Comunity area"
+    }, {
+        title: "Phone number"
+    }, {
+        title: "Property name"
+    }, {
+        title: "Zip code"
+    }, {
+        title: "Lat"
+    },{
+        title: "Long"
+    }]
+    });
+
+  // Listen click on the table
+  $('#' + id_table)
+    .on('click', 'tr', function () {
+      var data = table.row(this)
+        .data();
+      alert('You clicked on ' + data[0] + '\'s row');
+    });
+
+
+}
