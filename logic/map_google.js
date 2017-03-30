3e0b946bfdc9fe7bb10923a9bbdf1306d060a8b1
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-30 12:27:46 -0500

    Create function to place a marker on google maps.

--- logic/map_google.js
+++ logic/map_google.js
@@ -1,5 +1,10 @@
 'use strict'
-
+/*
+ * Depend on: no one local file
+ */
+ 
+let map_data
+let marker
 //Configuration map
 let config_mapa = {
   zoom: 4,
@@ -7,11 +12,21 @@ let config_mapa = {
   mapTypeId: 'terrain'
 }
 
+// Function for place a marker
+function createMarker(location) {
+    let data = {
+       position: location,
+       map: map_data,
+       title: 'Click to get more information'
+     }
+    //intance the marker
+    marker = new google.maps.Marker(data);
+}
+
 // Function for init a google maps map =}
 function initMap() {
   // Instance the map
   let div_map = document.getElementById("map")
-  div_map.innerHTML = "Paragraph changed!";
-  map = new google.maps.Map(div_map, config_mapa);
-
+  map_data = new google.maps.Map(div_map, config_mapa);
+  createMarker({lat: 41.244772343082076, lng: -103.7109375})
 }
