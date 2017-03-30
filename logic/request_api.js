2d4f00cbded7b052cc9bb2765748634ddd86bb95
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-30 10:48:34 -0500

    Create function for sending request that does not need headers.

--- logic/request_api.js
+++ logic/request_api.js
@@ -2,7 +2,7 @@
 
 let response;
 let dataset;
-//Create ajax JQuery request
+//Create ajax JQuery request with token
 function request(_method, _url) {
   $.ajax({
     method: _method,
@@ -20,9 +20,37 @@ function request(_method, _url) {
   })
 
   .fail((_, status, error) => {
-    console.log(status);
+    console.log(status)
+    console.log(error)
   });
 }
 
+//Create ajax JQuery request without token
+function request2(_method, _url) {
+  $.ajax({
+    method: _method,
+    url: _url
+  })
+  .done((res) => {
+    response = res;
+    //Fill the table with the answer
+    show_datasets_rent();
+    console.log(response);
+  })
+  .fail((_, status, error) => {
+    console.log(status)
+    console.log(error)
+  });
+}
+
+
+let opcion = 2
+
 //Testing the API
-request("GET", "https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets")
+if (opcion == 1){
+  let url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets"
+  request("GET", url)
+} else if(opcion == 2 ){
+  let url = "https://data.cityofchicago.org/resource/uahe-iimk.json"
+  request2("GET", url)
+}
