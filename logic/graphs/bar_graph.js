e346b39b092b5b6ce48ef5a1ab1d391951a10e93
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-31 22:05:38 -0500

    Create a listener for the bar chart.

--- logic/graphs/bar_graph.js
+++ logic/graphs/bar_graph.js
@@ -36,3 +36,20 @@ let config_bar = {
 
 let context_bar = document.getElementById("chart_bar");
 let mychart_bar = new Chart(context_bar, config_bar);
+
+
+// Events
+$("#chart_bar").click(
+  function(evt){
+    var activePoints = mychart_bar.getElementsAtEvent(evt);
+    var first_point = activePoints[0]
+    console.log(activePoints)
+    if(first_point){
+      var num_data_set = first_point._datasetIndex
+      var num_index_datum = first_point._index
+      var val = config_line.data.datasets[num_data_set].data[num_index_datum]
+      var lab = config_line.data.labels[num_index_datum]
+      console.log(lab + " "+ val)
+    }
+  }
+);
