880d2cb103ff809bad0e61aa98bbab7944d53a72
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-05 18:41:59 -0500

    Add wrappers for answer the request of the users.

--- logic/map_google.js
+++ logic/map_google.js
@@ -34,7 +34,7 @@ function createMarker(location, _icon) {
     marker = new google.maps.Marker(data);
 
     marker.info = new google.maps.InfoWindow({
-      content: '<b>Speed:</b> knots'
+      content: '<b>Speed:</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     });
 
     google.maps.event.addListener(marker, 'click', function() {
