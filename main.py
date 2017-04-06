b38148f50061bee02f648e0975333a8b9b5932a9
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-05 19:36:19 -0500

    Change references.

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 from flask import Flask, render_template
 
 app = Flask(__name__) 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/browser/')
+def browser ():
+    return render_template('browser.html')
+
+@app.route('/content/')
+def content():
+    return render_template('content.html')
+
+@app.route('/charts/')
+def charts():
+    return render_template('facade.html')
+
+if __name__ =="__main__":
+    app.run(debug=True)
+    
