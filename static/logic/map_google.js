61ac215bee370a353aa7287b305adabd81774adf
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-06 19:31:18 -0500

    Implement cool functionality of layers in the map  :metal: .

--- static/logic/map_google.js
+++ static/logic/map_google.js
@@ -11,6 +11,8 @@ let infowindow
 let icon_origin = '../../static/img/location_pin1.png'
 let icon_house = "../../static/img/location_pin2.png"
 
+let kml_limitations = "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=KML"
+
 
 //Configuration map
 let config_mapa = {
@@ -44,6 +46,19 @@ function createMarker(location, _icon) {
 
 }
 
+//function for show the different regions
+function loadKmlLayer(src, map) {
+  var kmlLayer = new google.maps.KmlLayer(src, {
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
+  });
+  google.maps.event.addListener(kmlLayer, 'click', function(event) {
+    var content = event.featureData.infoWindowHtml;
+    alert(content)
+  });
+}
+
 // Function for init a google maps map =}
 function initMap() {
   // Instance the map
@@ -51,5 +66,5 @@ function initMap() {
   map_data = new google.maps.Map(div_map, config_mapa);
   createMarker({lat: 41.870732, lng: -87.650495}, icon_origin)
   console.log("The map has been loaded.")
-
+  loadKmlLayer(kml_limitations, map_data);
 }
