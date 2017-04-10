5d4e2421c8b011903c79d55db786dc30c32414a6
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-09 23:03:29 -0500

    Compress video and add different versions.

--- static/logic/graphs/bar_graph.js
+++ static/logic/graphs/bar_graph.js
@@ -1,33 +1,12 @@
 data_labels= ["Daniel", "Julian", "Ana", "Beng", "Erika", "Jhon Doe"]
-data_values = [12, 19, 3, 5, 2, 3]
+data_values = []
 
 
 let config_bar = {
     type: 'bar', //bar, line
     data: {
         labels: data_labels,
-        datasets: [{
-            label: '# Something',
-            data: data_values,
-            borderWidth: 1,
-            fill: true,
-            backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-            borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                      ]
-        }]
+        datasets: []
     },
     options: {
         scales: {
@@ -49,9 +28,7 @@ let config_bar = {
 };
 
 let context_bar = document.getElementById("chart_bar");
-let mychart_bar = new Chart(context_bar, config_bar);
-
-
+let mychart_bar;
 
 $.ajax({
   method: "GET",
@@ -60,6 +37,34 @@ $.ajax({
 .done((res) => {
   console.log("Data graph have come.")
   console.log(res)
+  //Create chart when the data arrives
+
+    config_bar.data.datasets.push(
+    {
+        label: '# Something',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        fill: true,
+        backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+        borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                  ]
+    }
+  );
+  mychart_bar = new Chart(context_bar, config_bar);
+
 })
 .fail((_, status, error) => {
   console.log(status)
