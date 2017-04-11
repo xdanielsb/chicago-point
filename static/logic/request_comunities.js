a7b12ffcf3406c536bc664070d93158aa4048ae1
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-11 11:54:17 -0500

    Create images in COREL DRAW for the project.

--- static/logic/request_comunities.js
+++ static/logic/request_comunities.js
@@ -1,16 +1,25 @@
 'use strict'
 
 function create_table_health(response){
-  console.log("Creating table... =)")
+  console.log("Creating table=)")
 
   let result = jQuery.parseJSON(response)
-  _community_area = result["community_area"]
+  let _community_area = result["community_area"]
+  let _cancer_all_sites = result["cancer_all_sites"]
+  let _below_poverty_level = result["below_poverty_level"]
+  let _birth_rate = result["birth_rate"]
+  let _per_capita_income = result["per_capita_income"]
+  let _community_area_name = result["community_area_name"]
+  let _crowded_housing = result["crowded_housing"]
+  let _infant_mortality_rate = result["infant_mortality_rate"]
+  let _assault_homicide = result["assault_homicide"]
+  let _unemployment = result["unemployment"]
 
   var dataset = []
 
   // Iterate over the table
   for (var key in _community_area){
-    var row [key, _community_area[key]]
+    var row = [_community_area[key],  _community_area_name[key], _cancer_all_sites[key], _below_poverty_level[key],_birth_rate[key], _per_capita_income[key] , _crowded_housing[key], _infant_mortality_rate[key] , _assault_homicide[key], _unemployment[key]]
     dataset.push(row)
   }
 
@@ -21,8 +30,16 @@ function create_table_health(response){
   var table = $('#example').DataTable({
     data: dataset,
     columns: [
-      { title: "data0" },
-      { title: "data1" }
+      { title: "Community area code" },
+      { title: "Comunity area name" },
+      { title: "Rate Cancer" },
+      { title: "Below Poverty level" },
+      { title: "Birth Rate" },
+      { title: "Per Capita Income" },
+      { title: "Crowed housing" },
+      { title: "Infant mortality rate" },
+      { title: "Assaul homicide" },
+      { title: "unemployment" }
     ]
   });
 
@@ -37,7 +54,7 @@ function request_info_comunities(_method, _url) {
   })
   .done((res) => {
     console.log("The request for :"+_url+ " has finished");
-    insert_markers_locations(res)
+    create_table_health(res)
 
   })
   .fail((_, status, error) => {
@@ -46,4 +63,4 @@ function request_info_comunities(_method, _url) {
   });
 }
 
-request_location_houses("GET", "/info_houses/")
+request_info_comunities("GET", "/info_comunities/")
