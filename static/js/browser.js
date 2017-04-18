3fd55c02829dbd8556789a7fd327cecd050225bb
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-17 20:42:38 -0500

    Improve message alert, comment not working code.

--- static/js/browser.js
+++ static/js/browser.js
@@ -1,5 +1,5 @@
 if (navigator.userAgent.toLowerCase().indexOf('firefox') == -1) {
-	alert("User I recommend you to use Firefox.")
+	alert("Hi User, I hope that you are doing well!!!, for a better user experience I recommend you to use Firefox.")
 }
 $(document).ready(function(){
       $('.carousel').carousel();
