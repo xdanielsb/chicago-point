1692452abf64f1005d9d9b73fb5bc387ce1c94b2
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-23 14:27:35 -0500

    Improve design tables and notiifications desiign,
    User experience.

--- static/js/browser.js
+++ static/js/browser.js
@@ -1,6 +1,7 @@
-if (navigator.userAgent.toLowerCase().indexOf('firefox') == -1) {
-	alert("Hi User, I hope that you are doing well!!!, for a better user experience I recommend you to use Firefox.")
-}
+
 $(document).ready(function(){
-      $('.carousel').carousel();
+	if (navigator.userAgent.toLowerCase().indexOf('firefox') == -1) {
+		Materialize.toast('Hi User, I hope that you are doing well!!!, for a better user experience I recommend you to use Firefox.', 10000);
+	}
+  $('.carousel').carousel();
 });
--- static/js/content.js
+++ static/js/content.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+    Materialize.toast('Hello User, if you want further information of the property, click the property and you  will get more information as well as parks, health centers, police stations, libraries and  hospitals, Thanks for your visit.', 10000, 'rounded');
+
 });
