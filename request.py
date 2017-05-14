7d861a57f5bd522782358fcd161c95d0e016f0ee
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-14 18:06:04 -0500

    Clean and divide code in modules in order to make more open the project.

--- request.py
+++ request.py
@@ -1,30 +1,12 @@
-from urllib2 import urlopen, Request
 import json
 import urllib2
 import random
+import pandas as pd #Library for Data Science
+
+from urllib2 import urlopen, Request
 from operator import itemgetter #fancy sorted
-from math import radians, cos, sin, asin, sqrt
+from functions import haversine
 
-
-def haversine(lon1, lat1):
-    lon2, lat2 = [-87.650495, 41.870732]
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    km = 6367 * c
-    km = round(km, 3)
-    return km
-
-#Library for Data Science
-import pandas as pd
 
 class Request:
 
