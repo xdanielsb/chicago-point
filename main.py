4b4da58335ed7c2c9fc2584da7c2c5daf57a07d8
Daniel Santos <dfsantosbu@unal.edu.co>
2018-01-28 22:33:28 -0500

    Change port.

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def content():
 
 if __name__ =="__main__":
     print(__doc__)
-    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=80, threaded=True)
